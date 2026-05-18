e03c8b50dd0b173af2efc1a404e43cb30a9914d0
Lauren <lauren.stlazaro@gmail.com>
2026-05-18 14:41:13 -0300

    heurísticas contagem de peças e valor posicional

--- advsearch/your_agent/othello_minimax_count.py
+++ advsearch/your_agent/othello_minimax_count.py
@@ -4,37 +4,24 @@ from ..othello.gamestate import GameState
 from ..othello.board import Board
 from .minimax import minimax_move
 
-
-# Voce pode criar funcoes auxiliares neste arquivo
-# e tambem modulos auxiliares neste pacote.
-#
-# Nao esqueca de renomear 'your_agent' com o nome
-# do seu agente.
+MAX_DEPTH = 4
 
 
 def make_move(state) -> Tuple[int, int]:
     """
-    Returns a move for the given game state
-    :param state: state to make the move
-    :return: (int, int) tuple with x, y coordinates of the move (remember: 0 is the first row/column)
+    Returns a move for the given game state.
     """
 
-    # o codigo abaixo apenas retorna um movimento aleatorio valido para
-    # a primeira jogada 
-    # Remova-o e coloque uma chamada para o minimax_move (que vc implementara' no modulo minimax).
-    # A chamada a minimax_move deve receber sua funcao evaluate como parametro.
-
-    return random.choice([(2, 3), (4, 5), (5, 4), (3, 2)])
+    return minimax_move(state, MAX_DEPTH, evaluate_count)
 
 
-def evaluate_count(state, player:str) -> float:
-    """
-    Evaluates an othello state from the point of view of the given player. 
-    If the state is terminal, returns its utility. 
-    If non-terminal, returns an estimate of its value based on the number of pieces of each color.
-    :param state: state to evaluate (instance of GameState)
-    :param player: player to evaluate the state for (B or W)
-    """
+def evaluate_count(state, player: str) -> float:
 
+    adversary = Board.opponent(player)
 
-    return NotImplementedError
+    board = state.get_board()
+
+    player_count = board.num_pieces(player)
+    adversary_count = board.num_pieces(adversary)
+
+    return float(player_count - adversary_count)
--- advsearch/your_agent/othello_minimax_mask.py
+++ advsearch/your_agent/othello_minimax_mask.py
@@ -13,6 +13,9 @@ from .minimax import minimax_move
 # mask template adjusted from https://web.fe.up.pt/~eol/IA/MIA0203/trabalhos/Damas_Othelo/Docs/Eval.html
 # could optimize for symmetries but just put all values here for coding speed :P
 # DO NOT CHANGE! 
+
+MAX_DEPTH = 4
+
 EVAL_TEMPLATE = [
     [100, -30, 6, 2, 2, 6, -30, 100],
     [-30, -50, 1, 1, 1, 1, -50, -30],
@@ -27,26 +30,27 @@ EVAL_TEMPLATE = [
 
 def make_move(state) -> Tuple[int, int]:
     """
-    Returns a move for the given game state
-    :param state: state to make the move
-    :return: (int, int) tuple with x, y coordinates of the move (remember: 0 is the first row/column)
+    Returns a move for the given game state.
     """
 
-    # o codigo abaixo apenas retorna um movimento aleatorio valido para
-    # a primeira jogada 
-    # Remova-o e coloque uma chamada para o minimax_move (que vc implementara' no modulo minimax).
-    # A chamada a minimax_move deve receber sua funcao evaluate como parametro.
-
-    return random.choice([(2, 3), (4, 5), (5, 4), (3, 2)])
+    return minimax_move(state, MAX_DEPTH, evaluate_mask)
 
 
-def evaluate_mask(state, player:str) -> float:
-    """
-    Evaluates an othello state from the point of view of the given player. 
-    If the state is terminal, returns its utility. 
-    If non-terminal, returns an estimate of its value based on the positional value of the pieces.
-    You must use the EVAL_TEMPLATE above to compute the positional value of the pieces.
-    :param state: state to evaluate (instance of GameState)
-    :param player: player to evaluate the state for (B or W)
-    """
-    return 0   # substitua pelo seu codigo
+def evaluate_mask(state, player: str) -> float:
+
+    adversary = Board.opponent(player)
+
+    board = state.get_board().tiles
+
+    score = 0.0
+
+    for row in range(8):
+        for col in range(8):
+
+            if board[row][col] == player:
+                score += EVAL_TEMPLATE[row][col]
+
+            elif board[row][col] == adversary:
+                score -= EVAL_TEMPLATE[row][col]
+
+    return float(score)
